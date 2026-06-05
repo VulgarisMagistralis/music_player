bd3e305029d83c8196017edfe4a30cbafcc92245
VulgarisMagistralis <cenktuncbilek@gmail.com>
2026-06-05 11:26:30 -0400

    Filter out newline in auto increment script.

--- bump_version.sh
+++ bump_version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 PUBSPEC_FILE="pubspec.yaml"
-VERSION_CODE=$(sed -n 's/.*+[[:space:]]*\([0-9]\+\).*/\1/p' $PUBSPEC_FILE)
+VERSION_CODE=$(sed -n 's/.*+[[:space:]]*\([0-9]\+\).*/\1/p' $PUBSPEC_FILE |  tr -d '\n')
 if [ -z "$VERSION_CODE" ]; then
   echo "Error: Could not find version code"
   exit 1
 fi
 NEW_VERSION=$((VERSION_CODE + 1))
-sed -i "s/+$VERSION_CODE/+$NEW_VERSION/" $PUBSPEC_FILE
+sed -i "s|+$VERSION_CODE/+$NEW_VERSION/" $PUBSPEC_FILE
 echo "Version bumped from $VERSION_CODE to $NEW_VERSION"
