b2cb0d8aeb8eaea9b42e7c99c62ff0242b72979b
VulgarisMagistralis <cenktuncbilek@gmail.com>
2026-06-05 17:15:53 -0400

    Ralph iteration 1: work in progress

--- lib/low_level_wrapper/init.dart
+++ lib/low_level_wrapper/init.dart
@@ -1,3 +1,4 @@
+import 'package:music_player/low_level_wrapper/data/datasource/music_folder.dart';
 import 'package:music_player/low_level_wrapper/data/repository/folder_repo_imp.dart';
 import 'package:music_player/src/rust/frb_generated.dart';
 import 'package:path_provider/path_provider.dart';
@@ -10,7 +11,15 @@ class LowLevelInitializer {
     // ignore: invalid_use_of_internal_member
     await RustLib.instance.api.crateApiUtilsLoggerInitRustLogger();
     final applicationDirectory = (await getApplicationSupportDirectory()).path;
-    await LowLevelRepositoryImplementation().setDirectory(cacheDirectory: (await getExternalCacheDirectories())?.first.path ?? applicationDirectory, applicationDirectory: applicationDirectory);
+    final cacheDirectory = (await getExternalCacheDirectories())?.first.path ?? applicationDirectory;
+    await LowLevelRepositoryImplementation().setDirectory(cacheDirectory: cacheDirectory, applicationDirectory: applicationDirectory);
+    try {
+      final dataSource = LowLevelFolderDataSource();
+      final folders = await dataSource.loadFolders();
+      if (folders.isEmpty) {
+        await dataSource.saveFolders(folderList: ['/storage/emulated/0/Music']);
+      }
+    } catch (_) {}
     _initialized = true;
   }
 }
